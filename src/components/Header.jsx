6b2924854646e6951a6ac82034b0fe658426c468
dinesh1264 <dineshpandian700@gmail.com>
2025-04-13 22:56:58 +0530

    feat:implemented new ui for mobile nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 export const Header = () => {
+
   return (
     <div className="py-5 bg-[#1A1A1A] shadow-2xs shadow-gray-700">
-      <nav>
+      <nav className="phone-nav">
         <ol className="flex items-baseline-last justify-between mx-15 header-list">
           <li className="header text-6xl">
             <Link to="/" className=" p-0 m-0 neon-cyan">
@@ -42,9 +44,49 @@ export const Header = () => {
             </NavLink>
           </li>
         </ol>
+
+        <div className="phone-div">
+          <input type="checkbox" id="menu-toggle" className="peer hidden" />
+          <label htmlFor="menu-toggle" className="cursor-pointer">
+            <RxHamburgerMenu className="text-5xl text-teal-400 inline-block mb-4" />
+          </label>
+
+          <div className={`absolute peer-checked:block hidden px-2 pt-2 pb-2 mt-2 text-clip bg-black right-10 top-10`}>
+            <ul className="flex flex-col space-y-2 px-4 py-4">
+              <li>
+                <NavLink
+                  to="/tasks"
+                  className={({ isActive }) =>
+                    isActive ? "active-nav nav" : "nav"
+                  }
+                >
+                  Tasks
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/stats"
+                  className={({ isActive }) =>
+                    isActive ? "active-nav nav" : "nav"
+                  }
+                >
+                  Stats
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/roadmap"
+                  className={({ isActive }) =>
+                    isActive ? "active-nav nav" : "nav"
+                  }
+                >
+                  Roadmap
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
       </nav>
     </div>
-
-    
   );
 };
