f4d6586d72cf4c36829a989f27c9717e819cc24e
dinesh1264 <dineshpandian700@gmail.com>
2025-04-14 10:16:18 +0530

    fix:mobile nav ui fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,7 @@ import { RxHamburgerMenu } from "react-icons/rx";
 export const Header = () => {
 
   return (
-    <div className="py-5 bg-[#1A1A1A] shadow-2xs shadow-gray-700">
-      <nav className="phone-nav">
+    <div className="py-5 bg-[rgb(26,26,26)] shadow-2xs shadow-gray-70 w-full h-full">
         <ol className="flex items-baseline-last justify-between mx-15 header-list">
           <li className="header text-6xl">
             <Link to="/" className=" p-0 m-0 neon-cyan">
@@ -44,49 +43,6 @@ export const Header = () => {
             </NavLink>
           </li>
         </ol>
-
-        <div className="phone-div">
-          <input type="checkbox" id="menu-toggle" className="peer hidden" />
-          <label htmlFor="menu-toggle" className="cursor-pointer">
-            <RxHamburgerMenu className="text-5xl text-teal-400 inline-block mb-4" />
-          </label>
-
-          <div className={`absolute peer-checked:block hidden px-2 pt-2 pb-2 mt-2 text-clip bg-black right-10 top-10`}>
-            <ul className="flex flex-col space-y-2 px-4 py-4">
-              <li>
-                <NavLink
-                  to="/tasks"
-                  className={({ isActive }) =>
-                    isActive ? "active-nav nav" : "nav"
-                  }
-                >
-                  Tasks
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="/stats"
-                  className={({ isActive }) =>
-                    isActive ? "active-nav nav" : "nav"
-                  }
-                >
-                  Stats
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="/roadmap"
-                  className={({ isActive }) =>
-                    isActive ? "active-nav nav" : "nav"
-                  }
-                >
-                  Roadmap
-                </NavLink>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
     </div>
   );
 };
